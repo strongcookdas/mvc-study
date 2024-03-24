d68653ee7af8fca698992888e05d65aa57cc496e
strongcookdas <goodjav141592@gmail.com>
2024-03-24 21:15:05 +0900

    feat : 평균 학점 계산하는 메서드 구현

--- oop-credit-calculator/src/main/java/org/example/GradeCalculator.java
+++ oop-credit-calculator/src/main/java/org/example/GradeCalculator.java
@@ -5,11 +5,25 @@ import java.util.List;
 public class GradeCalculator {
     private final List<Course> courses;
 
-    public GradeCalculator(List<Course>courses) {
+    public GradeCalculator(List<Course> courses) {
         this.courses = courses;
     }
 
+    /*
+• 요구사항
+• 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+• MVC패턴(Model-View-Controller) 기반으로 구현한다.
+• 일급 컬렉션 사용
+ */
     public double calculate() {
-        return 4.5;
+        double multipliedCreditAndCourseGrads = 0;
+        for (Course course : courses) {
+            multipliedCreditAndCourseGrads += course.getCredit() * course.getGradeToNumber();
+        }
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrads / totalCompletedCredit;
     }
 }
