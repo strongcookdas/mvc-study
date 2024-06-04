cd3b07e3ad6f25a38d9d3c71704fe0ec45bdca11
strongcookdas <goodjav141592@gmail.com>
2024-06-04 15:11:36 +0900

    feat : 요청 파싱

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,9 +3,11 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -27,6 +29,21 @@ public class CustomWebApplicationServer {
             // 클라이언트 기다리기
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
+
+                /**
+                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 */
+
+                // 라인 바이 라인으로 읽어들이기 위해서 구현
+                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while (!Objects.equals(line = br.readLine(), "")) {
+                        System.out.println(line);
+                    }
+                }
             }
         }
     }
