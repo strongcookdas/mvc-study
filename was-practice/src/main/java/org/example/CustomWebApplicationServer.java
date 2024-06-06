4e0a18e3b957c4f4e95136bcd95434f5e695990f
strongcookdas <goodjav141592@gmail.com>
2024-06-06 19:25:39 +0900

    feat : response 객체 생성 후 전송 기능 구현

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -51,6 +51,11 @@ public class CustomWebApplicationServer {
                         int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
 
                         int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                        byte[] body = String.valueOf(result).getBytes();
+
+                        HttpResponse response = new HttpResponse(dos);
+                        response.response200Header("application/json",body.length);
+                        response.responseBody(body);
                     }
                 }
             }
