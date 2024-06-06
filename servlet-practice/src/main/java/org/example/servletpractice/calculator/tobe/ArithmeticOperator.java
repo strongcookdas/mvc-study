ba1aec3bd390aaf9736e2820a42227d1983ca0aa
strongcookdas <goodjav141592@gmail.com>
2024-06-07 02:52:53 +0900

    docs : 프로젝트 삭제

--- servlet-practice/src/main/java/org/example/servletpractice/calculator/tobe/ArithmeticOperator.java
+++
@@ -1,8 +0,0 @@
-package org.example.servletpractice.calculator.tobe;
-
-import org.example.servletpractice.calculator.domain.PositiveNumber;
-
-public interface ArithmeticOperator {
-    boolean supports(String operator);
-    int calculate(final PositiveNumber operand1, final PositiveNumber operand2);
-}
