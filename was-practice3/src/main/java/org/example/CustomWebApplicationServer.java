ca954d62ab729845fff75beaca003d09e31c5ae0
strongcookdas <goodjav141592@gmail.com>
2024-06-07 00:55:47 +0900

    feat : 스레드 풀 적용

--- was-practice3/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice3/src/main/java/org/example/CustomWebApplicationServer.java
@@ -6,9 +6,12 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -28,8 +31,10 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
 
-                //요청들어올 때마다 스레드 생성
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                /**
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                 */
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
